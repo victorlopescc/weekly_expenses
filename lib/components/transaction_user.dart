626cc3d43ea9ffb8b7a1790e14a86b89c943af09
victorlopescc <victorlopes.cc@gmail.com>
2023-06-10 11:25:10 -0300

    v0.4 - Refactoration

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import './transaction_list.dart';
+import './transaction_form.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({super.key});
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Running Shoes',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Electricity Bill',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(transactions: _transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
